93b9e6447b1213e379cfb65b3cf5e8bbb1fe6d32
Akanowo <ukoakanowo98@gmail.com>
2021-08-04 23:48:58 +0100

    fixed static folder

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const expres = require('express');
+const path = require('path')
 
 const app = expres();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(expres.static('public'));
+app.use(expres.json());
+app.use(expres.urlencoded({ extended: false }))
+app.use(expres.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/index.html')
